5a64a5dae5c8a628faf0b15dbf3f249787b260c1
Ali Adem <adema@augsburg.edu>
2025-11-24 14:46:52 -0600

    fixed init

--- racecontrol/__init__.py
+++ racecontrol/__init__.py
@@ -1,5 +1,5 @@
-from client import F1Client
-from jolpica import JolpicaProvider
-from fantasy import FantasyService
+from .client import F1Client
+from .jolpica import JolpicaProvider
+from .fantasy import FantasyService
 
 __all__ = ["F1Client", "JolpicaProvider", "FantasyService"]
