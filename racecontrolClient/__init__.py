69833493a6909ddde643d646070c439aac001ec4
Ali Adem <adema@augsburg.edu>
2025-12-02 20:35:17 -0600

    refactor

--- racecontrolClient/__init__.py
+++ racecontrolClient/__init__.py
@@ -1,4 +1,17 @@
-from .data.client import F1Client
-from .data.provider_jolpica import JolpicaProvider
+from .data.client import (
+    get_completed_round,
+    get_schedule,
+    get_driver_standings,
+    get_constructor_standings,
+    get_all_results_up_to,
+    get_fantasy_scores,
+)
 
-__all__ = ["F1Client", "JolpicaProvider"]
+__all__ = [
+    "get_completed_round",
+    "get_schedule",
+    "get_driver_standings",
+    "get_constructor_standings",
+    "get_all_results_up_to",
+    "get_fantasy_scores",
+]
